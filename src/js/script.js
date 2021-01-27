0c263e3ab49c11b47bfee5866d7ff93b3680d2a6
Patrick Karney <patrickkarney@gmail.com>
2021-01-27 14:06:24 +0100

    Add favorite class to books ex. 2

--- src/js/script.js
+++ src/js/script.js
@@ -7,21 +7,25 @@
 
     containerOf: {
       booksPanel: '.books-list',
-    }
+    },
+
+    book: {
+      image: '.books-list .book__image',
+    },
   };
 
   const templates = {
     book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
   };
 
+  const favoriteBooks = [];
+
   function render(){
 
-     
-    
     for(const data of dataSource.books){
       /*generate HTML based on template */
       const generateHTML = templates.book(data);
-      console.log('template', generateHTML);
+      
       /*create element using utils.createElementFromHTML */
       const elementHTML = utils.createDOMFromHTML(generateHTML);
       /*find menu container */
@@ -32,6 +36,27 @@
     }
   }
   
+  function initActions(){
+    const booksList = document.querySelectorAll(select.book.image);  
+    //for every element of book image in books-list
+    for(const element of booksList){
+      //add EventListener for every element on double klick
+      element.addEventListener('dblclick', function(event){
+        //prevent default
+        event.preventDefault();
+        //add this element to favoriteBooks
+        const bookId = element.getAttribute('data-id');
+        favoriteBooks.push(bookId);
+        //add class favorite
+        
+        if(bookId != null && bookId != favoriteBooks)element.classList.add('favorite');
+        
+        
+        console.log('ID\'s', favoriteBooks);
+      });
+    }
 
+  }
   render();
+  initActions();
 }
