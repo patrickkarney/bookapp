659346593da8dde3a612cd0dd956714228a4d478
Patrick Karney <patrickkarney@gmail.com>
2021-01-29 18:10:40 +0100

    Add rating ex. 6

--- src/js/script.js
+++ src/js/script.js
@@ -29,6 +29,10 @@
   function render(){
 
     for(const data of dataSource.books){
+
+      /*prepare const for rating bgc oraz width*/
+      data.ratingBgc = determineRatingBgc(data.rating);
+      data.ratingWidth = determineratingWidth(data.rating);
       /*generate HTML based on template */
       const generateHTML = templates.book(data);
       
@@ -109,11 +113,57 @@
         const book = document.querySelector('.book__image[data-id="' + element.id + '"]');
         book.classList.remove('hidden');
       }
-  
-      
-      
     }
   }
+
+  function determineRatingBgc(rating){
+    let background = '';
+    if(rating<=6) background = 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%);';
+    else if(rating>6 && rating<=8) background = 'linear-gradient(to bottom, #b4df5b 0%, #b4df5b 100%);';
+    else if(rating>8 && rating<=9) background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);';
+    else if(rating>9) background = 'linear-gradient(to bottom, #ff0084 0%, #ff0084 100%);';
+    console.log(background);
+    return background;
+  }
+
+  function determineratingWidth(rating) {
+    let Width = 0;
+    if (rating > 1) {
+      Width = 10;
+    }
+    if (rating > 2) {
+      Width = 20;
+    }
+    if (rating > 3) {
+      Width = 30;
+    }
+    if (rating > 4) {
+      Width = 40;
+    }
+    if (rating > 5) {
+      Width = 50;
+    }
+    if (rating > 6) {
+      Width = 60;
+    }
+    if (rating > 7) {
+      Width = 70;
+    }
+    if (rating > 8) {
+      Width = 80;
+    }
+    if (rating > 9) {
+      Width = 90;
+    }
+    if (rating > 10) {
+      Width = 100;
+    }
+  
+    return Width;
+  }
   render();
+  determineRatingBgc();
+  determineratingWidth();
+  
   initActions();
 }
